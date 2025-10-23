35aa0bac3458b4d523271d027231df79718abdf3
Mackenna <mackennamejdell@gmail.com>
2025-10-22 23:15:09 -0600

    update

--- display.py
+++ display.py
@@ -78,11 +78,11 @@ async def draw():
         red = int(255 * (1 - stat.lift_percent / 100))
         green = int(255 * (stat.lift_percent / 100))
         line_color = Color(red, green, 0)
-        matrix.drawLine(0, y_offset - 1, line_length, y_offset - 1, line_color)
+        matrix.drawLine(0, y_offset, line_length, y_offset, line_color)
 
-        # if stat.weather.icon_paths:
-        #     icon_path = stat.weather.icon_paths[half_seconds % len(stat.weather.icon_paths)]
-        #     matrix.setImage(icon_path, 17, y_offset)
+        if stat.weather.icon_paths:
+            icon_path = stat.weather.icon_paths[half_seconds % len(stat.weather.icon_paths)]
+            matrix.setImage(icon_path, 17, y_offset)
 
 
         matrix.drawText(30, y_offset, text_color, f'{stat.snowfall}"')
