744128a37c9c8ba921c14017411041551b90a71d
Mackenna <mackennamejdell@gmail.com>
2025-10-22 22:02:55 -0600

    update

--- display.py
+++ display.py
@@ -58,9 +58,9 @@ async def draw():
         y_offset = i * section_height
 
         matrix.drawText(0, y_offset, text_color, stat.short_name)
-        matrix.drawText(0, y_offset + 6, text_color, f'{stat.lift_percent} Open')
-        matrix.drawText(0, y_offset + 12, text_color, f'{stat.snowfall}"')
-        matrix.drawText(0, y_offset + 18, text_color, f'{stat.drive_time}')
+        matrix.drawText(6, y_offset, text_color, f'{stat.lift_percent} Open')
+        matrix.drawText(12, y_offset, text_color, f'{stat.snowfall}"')
+        matrix.drawText(18, y_offset, text_color, f'{stat.drive_time}')
 
 async def run_draw_loop():
     """Continuously draw the display."""
